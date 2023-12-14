9645422519ed7d870805bd133adbe8d0b3573de3
Yusuf Kaan SARI <yusufkaansari@hotmail.com>
2023-12-14 15:37:46 +0300

    Rotation

--- tetrisZ/Assets/Scripts/Piece.cs
+++ tetrisZ/Assets/Scripts/Piece.cs
@@ -7,11 +7,13 @@ public class Piece : MonoBehaviour
     public TetrominoData data { get; private set; }
     public Vector3Int position { get; private set; } // spawn position
     public Vector3Int[] cells { get; private set; }
+    public int rotationIndex { get; private set; }
     public void Initialize(Board board, Vector3Int position, TetrominoData data)
     {
         this.board = board;
         this.position = position;
         this.data = data;
+        this.rotationIndex = 0;
         if (this.cells == null)
         {
             this.cells = new Vector3Int[data.cells.Length];
@@ -24,14 +26,34 @@ public class Piece : MonoBehaviour
     private void Update()
     {
         this.board.Clear(this);
-        if (Input.GetKeyDown(KeyCode.A))
-        {
+        if (Input.GetKeyDown(KeyCode.Q)){
+            Rotate(-1);
+        }
+        else if (Input.GetKeyDown(KeyCode.E)){
+            Rotate(1);
+        }
+        if (Input.GetKeyDown(KeyCode.A)){
             Move(Vector2Int.left);
         } else if (Input.GetKeyDown(KeyCode.D))
         {
             Move(Vector2Int.right);
         }
+        if (Input.GetKeyDown(KeyCode.S)) 
+        {
+            Move(Vector2Int.down);
+        }
+        if(Input.GetKeyDown(KeyCode.Space)) 
+        {
+            HardDrop();
+        }
         this.board.Set(this);
+    }
+    private void HardDrop()
+    {
+        while (Move(Vector2Int.down))
+        {
+            continue;
+        }
     }
     private bool Move(Vector2Int translation)
     {
@@ -46,5 +68,42 @@ public class Piece : MonoBehaviour
             this.position = newPosition;
         }
         return valid;
+    }
+    private void Rotate(int direction)
+    {
+        this.rotationIndex = Wrap(this.rotationIndex + direction, 0, 4);
+
+        for (int i = 0; i < this.cells.Length; i++)
+        {
+            Vector3 cell = this.cells[i];
+            int x, y;
+            switch (this.data.tetromino)
+            {
+                case Tetromino.I:
+                case Tetromino.O:
+                    cell.x -= 0.5f;
+                    cell.y -= 0.5f;
+                    x = Mathf.CeilToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.CeilToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+                default:
+                    x = Mathf.RoundToInt((cell.x * Data.RotationMatrix[0] * direction) + (cell.y * Data.RotationMatrix[1] * direction));
+                    y = Mathf.RoundToInt((cell.x * Data.RotationMatrix[2] * direction) + (cell.y * Data.RotationMatrix[3] * direction));
+                    break;
+            }
+            cells[i] = new Vector3Int(x, y, 0);
+        }
+    }
+    //wrap: sarmak
+    private int Wrap(int input, int min, int max)
+    {
+        if (input < min)
+        {
+            return max - (min - input) % (max - min);   
+        }
+        else
+        {
+            return min + (input - min) % (max - min);
+        }
     }
 }
